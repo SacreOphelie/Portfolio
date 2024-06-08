83a613aab295d5c557d00abd3344bbc87c1af318
SacreOphelie <ophelie.sacre@outlook.be>
2024-06-08 17:07:58 +0200

    mentions

--- mentions.php
+++ mentions.php
@@ -7,60 +7,83 @@
     <title>Mentions Légales</title>
 </head>
 <body>
-    <h1>Mentions légales</h1>
-    <h3>1.Site</h3>
-    <p><a href="http://www.sacreophelie.be/">www.sacreophelie.be</a></p>
-    <h3>2.Editeur</h3>
-    <p>Ophélie Sacré</p>
-    <p>E-mail: ophelie.Sacre@outlook.be </p>
-    <h3>3.Hébergeur</h3>
-    Ce site est hébergé par o2switch.org, dont le siège social est situé à CHE DES PARDIAUX 63000 CLERMONT FERRAND.
-    <h2>Accès au site</h2>
-    <p>L’accès au site et son utilisation sont réservés à usage strictement personnel. Vous vous engagez à ne pas utiliser ce site et les informations ou données qui y figurent à des fins commerciales, politques, publicitaires et pour toute forme de sollicitation commerciale et notamment l’envoi de courriers électroniques non sollicités.</p>
-    <h2>Contenu du Site</h2>
-    <p>Toutes les marques, photographies, textes, commentaires, illustrations, images animées ou non, séquences vidéo, sons, ainsi que toutes les applications informatiques qui pourraient être utilisées pour faire fonctionner ce site et plus généralement tous les éléments reproduits ou utilisés sur le site sont protégés par les lois en vigueur au titre de la propriété intellectuelle. Ils sont la propriété pleine et entière de l’éditeur ou de ses partenaires. Toutes reproduction, représentation, utilisation ou adaptation, sous quelque forme que ce soit, de tout ou partie de ces éléments, y compris les applications informatiques, sans l’accord préalable et écrit de l’éditeur, sont strictement interdites. Le fait pour l’éditeur de ne pas engager de procédure dès la prise de connaissance de ces utilisations non autorisées ne vaut pas acceptation des dites utilisations et renonciation aux poursuites.</p>
-    <h2>Gestion du site</h2>
-    <p>Pour la bonne gestion du site, l'éditeur pourra à tout moment : - suspendre, interrompre ou limiter l'accès à tout ou partie du site, réserver l'accès au site, ou à certaines parties du site, à une catégorie déterminée d'internautes ; - supprimer toute information pouvant en perturber le fonctionnement ou entrant en contravention avec les lois nationales ou internationales ; - suspendre le site afin de procéder à des mises à jour.</p>
-    <h2>Responsabilités</h2>
-    <p>La responsabilité de l'éditeur ne peut être engagée en cas de défaillance, panne, difficulté ou interruption de fonctionnement, empêchant l'accès au site ou à une de ses fonctionnalités. Le matériel de connexion au site que vous utilisez est sous votre entière responsabilité. Vous devez prendre toutes les mesures appropriées pour protéger votre matériel et vos propres données notamment d'attaques virales par Internet. Vous êtes par ailleurs seul responsable des sites et données que vous consultez.
+<?php
+        include("headfoot/header.php");
+    ?>
 
-L'éditeur ne pourra être tenu responsable en cas de poursuites judiciaires à votre encontre :
-<ul>
-    <li>du fait de l'usage du site ou de tout service accessible via Internet ;</li>
-    <li>du fait du non-respect par vous des présentes conditions générales.</li>
-</ul>
+    <div class="mentions">
 
-L'éditeur n'est pas responsable des dommages causés à vous-même, à des tiers et/ou à votre équipement du fait de votre connexion ou de votre utilisation du site et vous renoncez à toute action contre lui de ce fait.
-
-Si l'éditeur venait à faire l'objet d'une procédure amiable ou judiciaire en raison de votre utilisation du site, il pourra se retourner contre vous pour obtenir l'indemnisation de tous les préjudices, sommes, condamnations et frais qui pourraient découler de cette procédure.</p>
-<h2>Liens hypertextes</h2>
-<p>La mise en place par les utilisateurs de tous liens hypertextes vers tout ou partie du site est autorisée par l'éditeur. Tout lien devra être retiré sur simple demande de l'éditeur. Toute information accessible via un lien vers d'autres sites n'est pas publiée par l'éditeur. L'éditeur ne dispose d'aucun droit sur le contenu présent dans ledit lien.</p>
-<h2>Collecte et protection des données</h2>
-<p>Vos données sont collectées par Ophélie Sacré
-
-Une donnée à caractère personnel désigne toute information concernant une personne physique identifiée ou identifiable (personne concernée) ; est réputée identifiable une personne qui peut être identifiée, directement ou indirectement, notamment par référence à un nom, un numéro d'identification ou à un ou plusieurs éléments spécifiques, propres à son identité physique, physiologique, génétique, psychique, économique, culturelle ou sociale. Les informations personnelles pouvant être recueillies sur le site sont principalement utilisées par l'éditeur pour la gestion des relations avec vous, et le cas échéant pour le traitement de vos demandes.
-
-Les données personnelles collectées sont les suivantes :
-<ul>
-    <li>Nom et prénom</li>
-    <li>adresse e-mail</li>
-</ul>
-</p>
-<h2>Droit d’accès, de rectification et de déréférencement de vos données</h2>
-<p>En application de la réglementation applicable aux données à caractère personnel, les utilisateurs disposent des droits suivants :
-
-le droit d’accès : ils peuvent exercer leur droit d'accès, pour connaître les données personnelles les concernant, en écrivant à l'adresse électronique suivante info.epse.be. Dans ce cas, avant la mise en œuvre de ce droit, la Plateforme peut demander une preuve de l'identité de l'utilisateur afin d'en vérifier l'exactitude ;
-le droit de rectification : si les données à caractère personnel détenues par la Plateforme sont inexactes, ils peuvent demander la mise à jour des informations ;
-le droit de suppression des données : les utilisateurs peuvent demander la suppression de leurs données à caractère personnel, conformément aux lois applicables en matière de protection des données ;
-le droit à la limitation du traitement : les utilisateurs peuvent de demander à la Plateforme de limiter le traitement des données personnelles conformément aux hypothèses prévues par le RGPD ;
-le droit de s’opposer au traitement des données : les utilisateurs peuvent s’opposer à ce que leurs données soient traitées conformément aux hypothèses prévues par le RGPD ;
-le droit à la portabilité : ils peuvent réclamer que la Plateforme leur remette les données personnelles qu'ils ont fournies pour les transmettre à une nouvelle Plateforme.
-
-Vous pouvez exercer ce droit en nous contactant, à l’adresse mail suivante :
-
-ophelie.sacre@outlook.be
-
-Toute demande doit être accompagnée de la photocopie d’un titre d’identité en cours de validité signé et faire mention de l’adresse à laquelle l'éditeur pourra contacter le demandeur. La réponse sera adressée dans le mois suivant la réception de la demande. Ce délai d'un mois peut être prolongé de deux mois si la complexité de la demande et /ou le nombre de demandes l'exigent.</p>
+        <h1>Mentions légales</h1>
+        <hr>
+        <h2>1.Site</h2>
+        <p><a href="http://www.sacreophelie.be/">www.sacreophelie.be</a></p>
+        <h2>2.Editeur</h2>
+        <p>Ophélie Sacré</p>
+        <p>E-mail: ophelie.Sacre@outlook.be </p>
+        <h2>3.Hébergeur</h2>
+        <p>Ce site est hébergé par o2switch.org, dont le siège social est situé à CHE DES PARDIAUX 63000 CLERMONT FERRAND.</p>
+        <h2>Accès au site</h2>
+        <p>L’accès au site et son utilisation sont réservés à usage strictement personnel. Vous vous engagez à ne pas utiliser ce site et les informations ou données qui y figurent à des fins commerciales, politques, publicitaires et pour toute forme de sollicitation commerciale et notamment l’envoi de courriers électroniques non sollicités.</p>
+        <h2>Contenu du Site</h2>
+        <p>Toutes les marques, photographies, textes, commentaires, illustrations, images animées ou non, séquences vidéo, sons, ainsi que toutes les applications informatiques qui pourraient être utilisées pour faire fonctionner ce site et plus généralement tous les éléments reproduits ou utilisés sur le site sont protégés par les lois en vigueur au titre de la propriété intellectuelle. Ils sont la propriété pleine et entière de l’éditeur ou de ses partenaires. Toutes reproduction, représentation, utilisation ou adaptation, sous quelque forme que ce soit, de tout ou partie de ces éléments, y compris les applications informatiques, sans l’accord préalable et écrit de l’éditeur, sont strictement interdites. Le fait pour l’éditeur de ne pas engager de procédure dès la prise de connaissance de ces utilisations non autorisées ne vaut pas acceptation des dites utilisations et renonciation aux poursuites.</p>
+        <h2>Gestion du site</h2>
+        <p>Pour la bonne gestion du site, l'éditeur pourra à tout moment : - suspendre, interrompre ou limiter l'accès à tout ou partie du site, réserver l'accès au site, ou à certaines parties du site, à une catégorie déterminée d'internautes ; - supprimer toute information pouvant en perturber le fonctionnement ou entrant en contravention avec les lois nationales ou internationales ; - suspendre le site afin de procéder à des mises à jour.</p>
+        <h2>Responsabilités</h2>
+        <p>La responsabilité de l'éditeur ne peut être engagée en cas de défaillance, panne, difficulté ou interruption de fonctionnement, empêchant l'accès au site ou à une de ses fonctionnalités. Le matériel de connexion au site que vous utilisez est sous votre entière responsabilité. Vous devez prendre toutes les mesures appropriées pour protéger votre matériel et vos propres données notamment d'attaques virales par Internet. Vous êtes par ailleurs seul responsable des sites et données que vous consultez.
+    
+    L'éditeur ne pourra être tenu responsable en cas de poursuites judiciaires à votre encontre :
+    <ul>
+        <li>du fait de l'usage du site ou de tout service accessible via Internet ;</li>
+        <li>du fait du non-respect par vous des présentes conditions générales.</li>
+    </ul>
+    
+    L'éditeur n'est pas responsable des dommages causés à vous-même, à des tiers et/ou à votre équipement du fait de votre connexion ou de votre utilisation du site et vous renoncez à toute action contre lui de ce fait.
+    
+    Si l'éditeur venait à faire l'objet d'une procédure amiable ou judiciaire en raison de votre utilisation du site, il pourra se retourner contre vous pour obtenir l'indemnisation de tous les préjudices, sommes, condamnations et frais qui pourraient découler de cette procédure.</p>
+    <h2>Liens hypertextes</h2>
+    <p>La mise en place par les utilisateurs de tous liens hypertextes vers tout ou partie du site est autorisée par l'éditeur. Tout lien devra être retiré sur simple demande de l'éditeur. Toute information accessible via un lien vers d'autres sites n'est pas publiée par l'éditeur. L'éditeur ne dispose d'aucun droit sur le contenu présent dans ledit lien.</p>
+    <h2>Collecte et protection des données</h2>
+    <p>Vos données sont collectées par Ophélie Sacré
+    
+    Une donnée à caractère personnel désigne toute information concernant une personne physique identifiée ou identifiable (personne concernée) ; est réputée identifiable une personne qui peut être identifiée, directement ou indirectement, notamment par référence à un nom, un numéro d'identification ou à un ou plusieurs éléments spécifiques, propres à son identité physique, physiologique, génétique, psychique, économique, culturelle ou sociale. Les informations personnelles pouvant être recueillies sur le site sont principalement utilisées par l'éditeur pour la gestion des relations avec vous, et le cas échéant pour le traitement de vos demandes.
+    
+    Les données personnelles collectées sont les suivantes :
+    <ul>
+        <li>Nom et prénom</li>
+        <li>adresse e-mail</li>
+    </ul>
+    </p>
+    <h2>Droit d’accès, de rectification et de déréférencement de vos données</h2>
+    <p>En application de la réglementation applicable aux données à caractère personnel, les utilisateurs disposent des droits suivants :
+    
+    le droit d’accès : ils peuvent exercer leur droit d'accès, pour connaître les données personnelles les concernant, en écrivant à l'adresse électronique suivante info.epse.be. Dans ce cas, avant la mise en œuvre de ce droit, la Plateforme peut demander une preuve de l'identité de l'utilisateur afin d'en vérifier l'exactitude ;
+    le droit de rectification : si les données à caractère personnel détenues par la Plateforme sont inexactes, ils peuvent demander la mise à jour des informations ;
+    le droit de suppression des données : les utilisateurs peuvent demander la suppression de leurs données à caractère personnel, conformément aux lois applicables en matière de protection des données ;
+    le droit à la limitation du traitement : les utilisateurs peuvent de demander à la Plateforme de limiter le traitement des données personnelles conformément aux hypothèses prévues par le RGPD ;
+    le droit de s’opposer au traitement des données : les utilisateurs peuvent s’opposer à ce que leurs données soient traitées conformément aux hypothèses prévues par le RGPD ;
+    le droit à la portabilité : ils peuvent réclamer que la Plateforme leur remette les données personnelles qu'ils ont fournies pour les transmettre à une nouvelle Plateforme.
+    
+    Vous pouvez exercer ce droit en nous contactant, à l’adresse mail suivante :
+    
+    ophelie.sacre@outlook.be
+    
+    Toute demande doit être accompagnée de la photocopie d’un titre d’identité en cours de validité signé et faire mention de l’adresse à laquelle l'éditeur pourra contacter le demandeur. La réponse sera adressée dans le mois suivant la réception de la demande. Ce délai d'un mois peut être prolongé de deux mois si la complexité de la demande et /ou le nombre de demandes l'exigent.</p>
+    <h2>Utilisation des données</h2>
+    <p>Les données personnelles collectées auprès des utilisateurs ont pour objectif la mise à disposition des services de la Plateforme, leur amélioration et le maintien d'un environnement sécurisé. La base légale des traitements est l’exécution du contrat entre l’utilisateur et la Plateforme. Plus précisément, les utilisations sont les suivantes :
+    
+    mise en œuvre d'une assistance pour les demandes ou questions posées par l'utilisateur</p>
+    <h2>Politique de conservation des données</h2>
+    <p>La Plateforme conserve vos données pour la durée nécessaire pour vous fournir ses services ou son assistance.
+    
+    Dans la mesure raisonnablement nécessaire ou requise pour satisfaire aux obligations légales ou réglementaires, régler des litiges, empêcher les fraudes et abus ou appliquer nos modalités et conditions, nous pouvons également conserver certaines de vos informations si nécessaire, même après que vous ayez fermé votre compte ou que nous n'ayons plus besoin pour vous fournir nos services.</p>
+    <h2>Photographies et représentation des produits</h2>
+    <p>
+    Les photographies de produits, accompagnant leur description, ne sont pas contractuelles et n'engagent pas l'éditeur.
+    </p>
+    <h2>Loi applicable</h2>
+    <p>Tout litige en relation avec l’utilisation du site https://www.sacreophelie.be y est soumis au droit belge. En dehors des cas où la loi ne le permet pas, il est fait attribution exclusive de juridiction aux tribunaux compétents de Belges.</p>
+    </div>
+    <?php include('headfoot/footer.php'); ?>
 
     
 </body>
